1ffcbced4436d26fe475d1a1c5de53cd5316e13f
r3ndd <3lliotn98@gmail.com>
2022-07-22 00:14:01 -0700

    first commit

---
+++ app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/r3ndd/urban-rogue/app/engine"
+	"github.com/r3ndd/urban-rogue/app/gui"
+)
+
+func main() {
+	engine.Init()
+	gui.Run()
+}
---
+++ app/modules/menu/menu.go
@@ -0,0 +1,9 @@
+package menu
+
+import (
+	"github.com/r3ndd/urban-rogue/app/gui"
+)
+
+func RunGUI() {
+	gui.Run()
+}
